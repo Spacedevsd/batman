55127d58272ca1888571510d6a07d70e74b0d501
Marcus Pereira <mpereirassa@gmail.com>
2020-02-09 21:55:12 -0300

    change name of template instance

--- run.py
+++ run.py
@@ -3,22 +3,22 @@ from batman import Batman
 
 
 batman = Batman()
-template = Template()
+view = Template()
 
 
 @batman.route("/character")
 def characters():
-    return template.render("character.html", {})
+    return view.render("character.html", {})
 
 
 @batman.route("/city")
 def city():
     context = {
         "name":  "Gothan City",
-        "hero": "Batman"    
+        "hero": "Batman"
     }
-    
-    return template.render("city.html", context)
+
+    return view.render("city.html", context)
 
 if __name__ == "__main__":
     batman.run(port=8000, reloader=True)
